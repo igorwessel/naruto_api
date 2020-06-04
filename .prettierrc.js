e389816635edfbd9af35b6404aad390d9ed4e015
igorwessel <igor_wessel@hotmail.com>
2020-06-04 13:52:16 -0300

    :sparkles: model for ninja.
    Not yet included relationships, i will work in this when finish others models.

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	arrowParens: 'avoid',
+	bracketSpacing: true,
+	insertPragma: false,
+	printWidth: 120,
+	proseWrap: 'never',
+	quoteProps: 'as-needed',
+	requirePragma: false,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'none',
+	useTabs: true
+};
--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,16 @@ import * as graphqlHTTP from 'express-graphql';
 
 import schema from './graphql/schema';
 class App {
-  public express: express.Application;
+	public express: express.Application;
 
-  constructor() {
-    this.express = express();
-    this.middleware();
-  }
+	constructor() {
+		this.express = express();
+		this.middleware();
+	}
 
-  private middleware(): void {
-    this.express.use(
-      '/graphql',
-      graphqlHTTP({ schema, graphiql: process.env.NODE_ENV === 'development' })
-    );
-  }
+	private middleware(): void {
+		this.express.use('/graphql', graphqlHTTP({ schema, graphiql: process.env.NODE_ENV === 'development' }));
+	}
 }
 
 export default new App().express;
