b98bd69a9270d21425b663168086788c7f39b86b
igorwessel <igor_wessel@hotmail.com>
2020-06-07 10:39:20 -0300

    refactor: removed resolver for ninja_attributes
    Removed because this resolver is not being used.

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 
 import { NinjaResolver } from './resolvers/NinjaResolver';
-import { NinjaAttrResolver } from './resolvers/NinjaAttrResolver';
 import { FamilyResolver } from './resolvers/FamilyResolver';
 import { NatureTypeResolver } from './resolvers/NatureTypeResolver';
 import { TeamResolver } from './resolvers/TeamResolver';
@@ -13,7 +12,7 @@ async function startServer(Container: any): Promise<express.Application> {
 	const app: express.Application = express();
 	const server = new ApolloServer({
 		schema: await buildSchema({
-			resolvers: [NinjaResolver, NinjaAttrResolver, FamilyResolver, NatureTypeResolver, TeamResolver],
+			resolvers: [NinjaResolver, FamilyResolver, NatureTypeResolver, TeamResolver],
 			container: Container
 		}),
 
--- src/resolvers/NinjaAttrResolver.ts
+++
@@ -1,5 +0,0 @@
-import { Resolver } from 'type-graphql';
-import { NinjaAttr } from '../entity/NinjaAttr';
-
-@Resolver(NinjaAttr)
-export class NinjaAttrResolver {}
