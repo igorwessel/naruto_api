272199d46ba6e20c7174f4b55c774379a28b7412
igorwessel <igor_wessel@hotmail.com>
2020-06-10 18:29:46 -0300

    feat: created basefilterinput and rename pagination args

---
+++ src/shared/PaginationArgs.ts
@@ -0,0 +1,22 @@
+import { ArgsType, Field, Int } from 'type-graphql';
+import { Min, Max } from 'class-validator';
+
+@ArgsType()
+export class PaginationArgs {
+	@Field(type => Int, { defaultValue: 0 })
+	@Min(0)
+	skip: number;
+
+	@Field(type => Int)
+	@Min(1)
+	@Max(30)
+	take: number = 15;
+
+	get startIndex(): number {
+		return this.skip;
+	}
+
+	get endIndex(): number {
+		return this.skip + this.take;
+	}
+}
