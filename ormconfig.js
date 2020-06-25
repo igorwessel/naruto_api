6a17fa9e5ba446afd527f514ffd405a5a9886a66
igorwessel <igor_wessel@hotmail.com>
2020-06-25 09:51:05 -0300

    config: add configs default for ORM, add CORS to project

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,13 @@
 module.exports = {
-	type: null, //YOUR DB
-	host: null, //HOST
-	port: null, //PORT FOR YOU DB
-	username: null, //USERNAME
-	password: null, //PASSWORD
-	database: null, //NAME OF YOUR SCHEMA
+	type: 'mysql', //
+	host: 'db', //HOST
+	port: '3306', //PORT FOR YOU DB
+	username: 'naruto', //USERNAME
+	password: 'sasuke', //PASSWORD
+	database: 'naruto_api_development', //NAME OF YOUR SCHEMA
 	entities: ['src/entity/**/**.ts'],
 	syncronize: process.env.NODE_ENV === 'development' ? true : false,
-	logging: true,
+	logging: process.env.NODE_ENV === 'development' ? true : false,
 	migrations: ['src/migrations/**/**.ts'],
 	cli: {
 		migrationsDir: ['src/migrations/']
