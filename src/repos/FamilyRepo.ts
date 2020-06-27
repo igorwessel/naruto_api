8f7f387518529dc4f7d2ce692ba665e41b015d3a
igorwessel <igor_wessel@hotmail.com>
2020-06-27 12:31:48 -0300

    feat: route to get family by ninja name
    Created middleware to treat ninja name before send query to db

--- src/repos/FamilyRepo.ts
+++ src/repos/FamilyRepo.ts
@@ -3,10 +3,11 @@ import { Family } from '../entity/Family';
 
 @EntityRepository(Family)
 export class FamilyRepo extends Repository<Family> {
-	async getByNinjaID(id: number) {
+	async getByNinjaIDOrName(id?: number, name?: string) {
 		const family = await this.createQueryBuilder('family')
-			.innerJoinAndSelect('family.parent_to', 'parent_to')
-			.where('parentFromId = :id', { id })
+			.innerJoin('family.parent_to', 'parent_to')
+			.innerJoin('family.parent_from', 'parent_from')
+			.where(`${id ? 'parentFromId = :parameter' : 'parent_from.name = :parameter'}`, { parameter: id ? id : name })
 			.select(['parent_to.id as "id"', 'family.relationship as "relationship"', 'parent_to.name as "name"'])
 			.getRawMany();
 
