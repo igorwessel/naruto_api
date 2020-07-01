053dee692c6b7b2d5eef0a8d8a67b4f4624b9d49
igorwessel <igor_wessel@hotmail.com>
2020-07-01 06:09:32 -0300

    packages: add restful and database packages

--- mix.exs
+++ mix.exs
@@ -22,6 +22,15 @@ defmodule NarutoAPI.MixProject do
   defp deps do
     [
 
+      # RESTFul
+      {:plug, "~> 1.8"},
+      {:cowboy, "~> 2.6"},
+      {:plug_cowboy, "~> 2.0"},
+
+      # Database
+      {:myxql, "~> 0.3.0"},
+      {:ecto, "~> 3.1"},
+      {:ecto_sql, "~> 3.1"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
