081a28962d8edbe7162bd68477498b7a5534923a
igorwessel <igor_wessel@hotmail.com>
2020-07-01 07:30:15 -0300

    chore: config for db

--- config/dev.exs
+++ config/dev.exs
@@ -6,3 +6,12 @@ config :logger, level: :debug
 # Configures application
 config :naruto_api, NarutoAPI.Endpoint,
   port: 4000
+
+  # Configure your database
+config :naruto_api, NarutoAPI.Repo,
+  timeout: 99_999_999,
+  pool: Ecto.Adapters.SQL
+  username: System.get_env("DB_USER"),
+  password: System.get_env("DB_PASSWORD"),
+  database: System.get_env("DB_NAME"),
+  port: System.get_env("DB_PORT")
