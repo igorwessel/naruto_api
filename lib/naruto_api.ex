208ccdba2aa761da135704be1ced84cace56047c
igorwessel <igor_wessel@hotmail.com>
2020-07-01 06:14:23 -0300

    chore: removed default function.

--- lib/naruto_api.ex
+++ lib/naruto_api.ex
@@ -2,17 +2,4 @@ defmodule NarutoAPI do
   @moduledoc """
   Documentation for `NarutoAPI`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> NarutoAPI.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
