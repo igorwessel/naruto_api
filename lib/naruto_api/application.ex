f596eb27de467bab173ff99e38c74f7b8333bc91
igorwessel <igor_wessel@hotmail.com>
2020-07-01 06:48:06 -0300

    feat: initial config to application

---
+++ lib/naruto_api/application.ex
@@ -0,0 +1,28 @@
+defmodule NarutoAPI.Application do
+  require Logger
+  use Application
+
+  import Supervisor.Spec
+
+  def start(_type, _args), do: Supervisor.start_link(children(), opts())
+
+  defp children(), do: [
+    supervisor(NarutoAPI.Repo, []),
+    {Plug.Cowboy, scheme: :http, plug: NarutoAPI.Endpoint, options: get_config()},
+  ]
+
+  defp opts(), do: [
+    strategy: :one_for_one,
+    name: NarutoAPI.Supervisor
+  ]
+
+  defp get_config() do
+    Logger.info("Starting NarutoAPI application")
+
+    with {:ok, config} <- Application.fetch_env(:naruto_api, NarutoAPI.Endpoint) do
+      port = Environments.get_env(config[:port])
+      Logger.info("Starting server using port #{port}")
+      config
+    end
+  end
+end
