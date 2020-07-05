dd9649634fac2ceeb47818197b0f3b8e55c67d32
igorwessel <igor_wessel@hotmail.com>
2020-07-05 12:23:58 -0300

    chore: remove useless messages

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
 import { startServer } from './app';
 import { connect } from './config/typeorm';
 
-import express from 'express';
+import * as express from 'express';
 import Container from 'typedi';
 import { useContainer } from 'typeorm';
 
-async function main(): Promise<void> {
+async function main(): Promise<express.Application> {
 	useContainer(Container);
 	connect();
 	const app: express.Application = await startServer();
 
-	app.listen(8000);
-	console.log(`Server start on `, 8000);
-	console.log(`REST -> http://localhost:8000/v1/rest`);
-	console.log(`GRAPHQL -> http://localhost:8000/v1/graphql`);
+	app.listen(8000, () =>
+		console.log(
+			'\x1b[34m%s\x1b[0m',
+			`
+		REST      → http://localhost:8000/v1/rest/
+		GraphQL   → http://localhost:8000/v1/graphql
+	`
+		)
+	);
+	return app;
 }
 
-main();
+const server = main();
+
+export default server;
