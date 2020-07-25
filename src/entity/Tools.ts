feeb6b20e8b0135d7c0687f91ccb33b1de0aeba7
Igor Wessel <igor_wessel@hotmail.com>
2020-07-25 07:20:30 -0300

    feat: add new names to tools

--- src/entity/Tools.ts
+++ src/entity/Tools.ts
@@ -1,11 +1,35 @@
-import { Entity, OneToMany } from 'typeorm';
+import { Entity, OneToMany, Column } from 'typeorm';
 import { NinjaTools } from './Ninja';
-import { ObjectType } from 'type-graphql';
+import { ObjectType, Field } from 'type-graphql';
 import { BaseContent } from '../shared/BaseContent';
 
 @ObjectType()
 @Entity()
 export class Tools extends BaseContent {
+	@Field()
+	@Column({ nullable: true })
+	kanji: string;
+
+	@Field()
+	@Column({ nullable: true })
+	romaji: string;
+
+	@Field()
+	@Column({ nullable: true })
+	portugues: string;
+
+	@Field()
+	@Column({ nullable: true })
+	games: string;
+
+	@Field()
+	@Column({ nullable: true })
+	manga_panini: string;
+
+	@Field()
+	@Column({ nullable: true })
+	tv_brasileira: string;
+
 	@OneToMany(type => NinjaTools, ninja_tools => ninja_tools.tools)
 	ninja_tools: NinjaTools[];
 }
