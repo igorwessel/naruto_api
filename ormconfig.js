67f40b69501a1c2b3017da7e3b91779c333f94dd
Igor Wessel <igor_wessel@hotmail.com>
2020-12-18 10:46:11 -0300

    chore: add db port to .env and change db in docker

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,8 @@
 module.exports = {
 	type: 'mysql', //
 	host: 'localhost', //HOST
-	database: 'naruto_api_development', //NAME OF YOUR SCHEMA
-	port: 3306, //PORT FOR YOU DB
+	database: 'naruto_api', //NAME OF YOUR SCHEMA
+	port: process.env.DB_PORT || 3306, //PORT FOR YOU DB
 	username: process.env.DB_USER || 'root', //USERNAME
 	password: process.env.DB_PASSWORD || '', //PASSWORD
 	entities: ['./build/entity/**/*.js'],
