960dd332f5fdf7cf77a8c3075fc501b4d1f75476
Igor Wessel <igor_wessel@hotmail.com>
2020-12-18 10:40:17 -0300

    fix: removed unused joins

--- src/repos/NinjaJutsuRepo.ts
+++ src/repos/NinjaJutsuRepo.ts
@@ -8,22 +8,24 @@ export class NinjaJutsuRepo extends Repository<NinjaJutsu> {
 			.leftJoin('ninja_jutsu.jutsu', 'jutsu')
 			.leftJoin('ninja_jutsu.ninja', 'ninja')
 			.leftJoin('jutsu.nature_type', 'nature_type')
+			/* 
 			.leftJoin('jutsu.related_jutsu', 'related_jutsu')
 			.leftJoin('jutsu.parent_jutsu', 'parent_jutsu')
 			.leftJoin('jutsu.derived_jutsu', 'derived_jutsu')
+			*/
 			.addSelect('jutsu.id')
 			.addSelect('jutsu.name')
-			.addSelect('jutsu.literal_english_name')
-			.addSelect('jutsu.english_anime_name')
 			.addSelect('jutsu.description ')
 			.addSelect('jutsu.range')
 			.addSelect('jutsu.hand_seals')
+			/*
 			.addSelect('related_jutsu.id')
 			.addSelect('related_jutsu.name')
 			.addSelect('parent_jutsu.id')
 			.addSelect('parent_jutsu.name')
 			.addSelect('derived_jutsu.id')
 			.addSelect('derived_jutsu.name')
+			*/
 			.addSelect('ninja_jutsu.only ')
 			.addSelect('nature_type.name ')
 			.where(`${id ? 'ninja_jutsu.ninjaId = :parameter' : 'ninja.name = :parameter'}`, { parameter: id ? id : name })
