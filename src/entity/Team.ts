36a98664497af0eca5b2849688f25b511fb41ac3
Igor Wessel <igor_wessel@hotmail.com>
2021-08-30 17:49:28 -0300

    wip: remove typeorm from entities

--- src/entity/Team.ts
+++ src/entity/Team.ts
@@ -1,15 +1,12 @@
 import { BaseManyToMany } from '../shared/BaseManyToMany';
-import { Column, Entity, OneToMany, ManyToMany, JoinTable } from 'typeorm';
 import { ObjectType, Field } from 'type-graphql';
-import { NinjaTeam, Ninja } from './Ninja';
+import { Ninja } from './Ninja';
 import { Affiliation } from './Affiliation';
 import { Jutsu } from './Jutsu';
 
 @ObjectType()
-@Entity()
 export class Team extends BaseManyToMany {
 	@Field({ nullable: true })
-	@Column('text', { nullable: true })
 	description: String;
 
 	@Field(() => Boolean)
@@ -22,15 +19,8 @@ export class Team extends BaseManyToMany {
 	leaders: Ninja[];
 
 	@Field(() => Affiliation, { nullable: true })
-	@ManyToMany(type => Affiliation)
-	@JoinTable({ name: 'team_affiliation' })
 	affiliation: Affiliation[];
 
 	@Field(() => Jutsu, { nullable: true })
-	@ManyToMany(type => Jutsu)
-	@JoinTable({ name: 'team_jutsu' })
 	jutsu: Jutsu[];
-
-	@OneToMany(type => NinjaTeam, ninjateam => ninjateam.team)
-	has_team: NinjaTeam[];
 }
