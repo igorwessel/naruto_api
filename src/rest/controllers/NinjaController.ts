207e0e4a81254a94c8d964631e1cfcad67a0e708
Igor Wessel <igor_wessel@hotmail.com>
2021-09-01 08:07:06 -0300

    refactor: use baseRoute in controllers

--- src/rest/controllers/NinjaController.ts
+++ src/rest/controllers/NinjaController.ts
@@ -7,13 +7,13 @@ import { AttributeNotFound } from '../errors/AttributesError';
 
 import { NinjaWithAllRelations } from '../../shared/Ninja';
 
-import { treatmentName } from '../middlewares/NinjasMiddlewares';
+import { treatmentName } from '../middlewares/HelpersMiddlewares';
 
 import { prisma } from '../../prisma';
 
-@JsonController()
+@JsonController('/ninjas')
 export class NinjaController {
-	@Get('/ninjas')
+	@Get('/')
 	@OnUndefined(NinjaNotFoundError)
 	async getAll(@QueryParams({ required: false }) { name, limit, offset, clan, sex }: NinjaQueryParams) {
 		const ninjas = await prisma.ninja.findMany({
@@ -56,7 +56,7 @@ export class NinjaController {
 		}));
 	}
 
-	@Get('/ninjas/:id([0-9]+)')
+	@Get('/:id([0-9]+)')
 	@OnUndefined(NinjaNotFoundError)
 	getNinjaByID(@Param('id') id: number) {
 		return prisma.ninja.findUnique({
@@ -65,7 +65,7 @@ export class NinjaController {
 		});
 	}
 
-	@Get('/ninjas/:id([0-9]+)/attributes')
+	@Get('/:id([0-9]+)/attributes')
 	@OnUndefined(AttributeNotFound)
 	async getAttributes(@Param('id') id: number) {
 		const attributes = await prisma.ninja.findUnique({ where: { id } }).ninjaAttr({
@@ -77,7 +77,7 @@ export class NinjaController {
 		return attributes;
 	}
 
-	@Get('/ninjas/:id([0-9]+)/jutsus')
+	@Get('/:id([0-9]+)/jutsus')
 	async getJutsus(@Param('id') id: number) {
 		const jutsus = await prisma.ninja.findUnique({ where: { id } }).jutsus({
 			include: { nature_type: true }
@@ -88,7 +88,7 @@ export class NinjaController {
 		return jutsus;
 	}
 
-	@Get('/ninjas/:id([0-9]+)/family')
+	@Get('/:id([0-9]+)/family')
 	async getFamily(@Param('id') id: number) {
 		const family = await prisma.ninja
 			.findUnique({ where: { id } })
@@ -99,7 +99,7 @@ export class NinjaController {
 		return family;
 	}
 
-	@Get('/ninjas/:id([0-9]+)/tools')
+	@Get('/:id([0-9]+)/tools')
 	async getTools(@Param('id') id: number) {
 		const tools = await prisma.ninja.findUnique({ where: { id } }).tools();
 
@@ -108,7 +108,7 @@ export class NinjaController {
 		return tools;
 	}
 
-	@Get('/ninjas/:id([0-9]+)/teams')
+	@Get('/:id([0-9]+)/teams')
 	async getTeams(@Param('id') id: number) {
 		const teams = await prisma.ninja.findUnique({ where: { id } }).team();
 
@@ -117,7 +117,7 @@ export class NinjaController {
 		return teams;
 	}
 
-	@Get('/ninjas/:name([A-z_]+)')
+	@Get('/:name([A-z_]+)')
 	@UseBefore(treatmentName)
 	@OnUndefined(NinjaNotFoundError)
 	async getNinjaByName(@Param('name') name: string) {
@@ -127,7 +127,7 @@ export class NinjaController {
 		});
 	}
 
-	@Get('/ninjas/:name([A-z_]+)/jutsus')
+	@Get('/:name([A-z_]+)/jutsus')
 	@UseBefore(treatmentName)
 	async getJutsusByNinjaName(@Param('name') name: string) {
 		const jutsus = await prisma.ninja
@@ -141,7 +141,7 @@ export class NinjaController {
 		return jutsus;
 	}
 
-	@Get('/ninjas/:name([A-z_]+)/attributes')
+	@Get('/:name([A-z_]+)/attributes')
 	@UseBefore(treatmentName)
 	async getAttributesByName(@Param('name') name: string) {
 		const attributes = await prisma.ninja
@@ -157,7 +157,7 @@ export class NinjaController {
 		return attributes;
 	}
 
-	@Get('/ninjas/:name([A-z_]+)/family')
+	@Get('/:name([A-z_]+)/family')
 	@UseBefore(treatmentName)
 	async getFamilyByNinjaName(@Param('name') name: string) {
 		const family = await prisma.ninja
@@ -167,7 +167,7 @@ export class NinjaController {
 		return family;
 	}
 
-	@Get('/ninjas/:name([A-z_]+)/tools')
+	@Get('/:name([A-z_]+)/tools')
 	@UseBefore(treatmentName)
 	async getToolsByName(@Param('name') name: string) {
 		const tools = await prisma.ninja.findFirst({ where: { name: { contains: name } } }).tools();
@@ -177,7 +177,7 @@ export class NinjaController {
 		return tools;
 	}
 
-	@Get('/ninjas/:name([A-z_]+)/teams')
+	@Get('/:name([A-z_]+)/teams')
 	@UseBefore(treatmentName)
 	async getTeamsByName(@Param('name') name: string) {
 		const teams = await prisma.ninja.findFirst({ where: { name: { contains: name } } }).team();
