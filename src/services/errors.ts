8ee5fd6a117e5faae5cb746ebdc3d5272e741604
Igor Wessel <igor_wessel@hotmail.com>
2022-03-18 06:59:41 -0300

    feat: transform exception in common error

--- src/services/errors.ts
+++ src/services/errors.ts
@@ -1,13 +1,20 @@
-type DefaultErrorInput = {
+import { pipe } from 'fp-ts/function'
+import * as E from 'fp-ts/Either'
+
+export type DefaultError = {
   error: string
   statusCode: number
   message: string
 }
 
-export const makeErrorOutput = ({ error, statusCode, message }: DefaultErrorInput) => ({
-  statusCode,
-  error,
-  message,
-})
+export const isDefaultError = (error: unknown): error is DefaultError => 'statusCode' in (error as DefaultError)
+
+export const makeErrorOutput = (err: DefaultError | unknown): DefaultError => {
+  if (isDefaultError(err)) {
+    return err
+  }
+
+  return pipe(err, E.toError, err => ({ error: err.name, statusCode: 500, message: err.message }))
+}
 
 export const notFoundError = (message: string) => makeErrorOutput({ error: 'Not Found', statusCode: 404, message })
