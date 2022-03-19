af26e27d43acca0b8fef8c87c26608df44a10dc2
Igor Wessel <igor_wessel@hotmail.com>
2022-03-19 17:55:58 -0300

    feat: add /jutsus routes

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import fastifyCors from 'fastify-cors'
 import { routes as ninjasRoutes } from '~/routes/ninjas'
 import { routes as toolsRoutes } from '~/routes/tools'
 import { routes as teamsRoutes } from '~/routes/teams'
+import { routes as jutsusRoutes } from '~/routes/jutsus'
 
 import prismaPlugin from '~/plugins/prisma'
 
@@ -14,6 +15,7 @@ app.register(prismaPlugin)
 
 app.register(toolsRoutes, { prefix: '/api/v1/tools' })
 app.register(teamsRoutes, { prefix: '/api/v1/teams' })
+app.register(jutsusRoutes, { prefix: '/api/v1/jutsus' })
 app.register(ninjasRoutes, { prefix: '/api/v1/ninjas' })
 
 app.listen(process.env.PORT ?? 3000, '0.0.0.0').catch(err => {
---
+++ src/routes/jutsus.ts
@@ -0,0 +1,78 @@
+import { FastifyInstance } from 'fastify'
+
+import * as t from 'io-ts'
+import * as TE from 'fp-ts/TaskEither'
+import { pipe } from 'fp-ts/function'
+
+import { getJutsu, getJutsus, getNinja } from '~/handlers/jutsus'
+
+import { validatorCompiler } from '~/services/request_validator'
+import { paginationCodec, PaginationType } from '~/types/pagination'
+import { idCodec, kebabCaseCodec } from '~/types/params'
+
+type JutsuParam = {
+  jutsu: string
+}
+
+const paramsType = t.type({
+  jutsu: t.union([idCodec, kebabCaseCodec]),
+})
+
+type ParamsType = t.TypeOf<typeof paramsType>
+
+export const routes = async (app: FastifyInstance): Promise<FastifyInstance> =>
+  app
+    .get<
+      {
+        Querystring: PaginationType
+      },
+      unknown,
+      t.Type<PaginationType>
+    >(
+      '/',
+      {
+        schema: {
+          querystring: paginationCodec,
+        },
+        validatorCompiler: validatorCompiler(),
+      },
+      (req, reply) => {
+        pipe(
+          getJutsus(reply, { limit: parseInt(req.query.limit ?? '15'), offset: parseInt(req.query.offset ?? '0') }),
+          TE.map(jutsus => reply.send(jutsus)),
+          TE.mapLeft(err => reply.send(err))
+        )()
+      }
+    )
+    .get<{ Params: JutsuParam }, unknown, t.Type<ParamsType>>(
+      '/:jutsu',
+      {
+        schema: {
+          params: paramsType,
+        },
+        validatorCompiler: validatorCompiler(),
+      },
+      (req, reply) => {
+        pipe(
+          getJutsu(reply, req.params.jutsu),
+          TE.map(jutsu => reply.send(jutsu)),
+          TE.mapLeft(err => reply.send(err))
+        )()
+      }
+    )
+    .get<{ Params: JutsuParam }, unknown, t.Type<ParamsType>>(
+      '/:jutsu/ninjas',
+      {
+        schema: {
+          params: paramsType,
+        },
+        validatorCompiler: validatorCompiler(),
+      },
+      (req, reply) => {
+        pipe(
+          getNinja(reply, req.params.jutsu),
+          TE.map(ninjas => reply.send(ninjas)),
+          TE.mapLeft(err => reply.send(err))
+        )()
+      }
+    )
