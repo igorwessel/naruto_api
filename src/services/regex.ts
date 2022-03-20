31e64b690fb25a7d7c1900ea569d1480f4d49231
Igor Wessel <igor_wessel@hotmail.com>
2022-03-20 08:40:12 -0300

    fix: kebab-case regex accept unicode characters

--- src/services/regex.ts
+++ src/services/regex.ts
@@ -1,9 +1,9 @@
 export const ID = '(^[0-9]*$)'
 
-export const KEBAB_CASE = '(^[a-z0-9]+((-[a-z0-9]+){1,})?$)'
+export const KEBAB_CASE = '^([\\p{Ll}0-9]*)(-[\\p{Ll}0-9]+)*$'
 
 export function isKebabCase(value: string) {
-  return new RegExp(KEBAB_CASE).test(value)
+  return new RegExp(KEBAB_CASE, 'u').test(value)
 }
 
 export function isID(value: string) {
